eaf336d16896f112530ad59d13c89b1bec1dfc0d
Mohammed Khalifah <tu4205521@taibahu.edu.sa>
2023-11-20 21:00:39 +0300

    make rate function in employee class

--- pages/view_rate.php
+++ pages/view_rate.php
@@ -28,33 +28,13 @@ function getAllVolunteers($volunteering_id, $conn){
 $volunteers = getAllVolunteers($volunteering_id,$conn);
 
 
+
+
 // if he saved
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (isset($_POST['save'])){
-        // لو كان مقيهم قبل كدا
-        $sql2 = "SELECT is_rated FROM Volunteering WHERE id=$volunteering_id AND is_rated>0 ;";
-        $result = mysqli_query($conn,$sql2);
-        if (mysqli_num_rows($result) > 0) {
-            echo '<script type=text/javascript> alert("You have Rated this Volunteering opportunity before!");window.location.href=window.location.href;</script>';
-        }else{
-            foreach($volunteers as $volunteer){
-                $rate = $_POST['rate_'.htmlspecialchars($volunteer['id'])];
-                // echo $volunteer['name'].$rate;
-                $id = $volunteer["id"];
-                $sql = "UPDATE volunteer
-                SET rates = rates + $rate
-                WHERE `volunteer`.`id` = $id ;";
-                if(!mysqli_query($conn,$sql)){
-                    echo "error in updating rate".mysqli_error($conn);
-                }
-            }
-            $sql3 = "UPDATE Volunteering SET is_rated=1 WHERE id=$volunteering_id";
-            if(!mysqli_query($conn,$sql3)){
-                echo "error and updating is_rated".mysqli_error($conn);
-            }
-            echo '<script type=text/javascript> alert("You have Successfully Rated this Volunteering opportunity!");window.location.href=window.location.href;</script>';
-        }
+        $employee->saveRates($volunteers,$volunteering_id,$conn);
     }
 }
 
